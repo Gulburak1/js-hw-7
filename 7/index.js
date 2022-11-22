9c99a990efab802b8a6d8803418ade39901610cc
Gulburak1 <116088697+Gulburak1@users.noreply.github.com>
2022-11-22 17:56:15 +0600

    Update index.js

--- 7/index.js
+++ 7/index.js
@@ -18,4 +18,30 @@ let alinaTable = alina.getSalary();
 
 console.log(nurbekTable + alinaTable);
 
-
+class MyString {
+    reverse(str) {
+        let reversStr = '';
+        for (let i = str.length - 1; i >= 0; i--) {
+            reversStr += str[i];
+        }
+        return reversStr;
+    }
+    ucFirst(str) {
+        let newStr = str[0].toUpperCase() + str.slice(1);
+        return newStr;
+    }
+    ucWords(str) {
+        let arr = str.split(' ');
+        let strArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            let a = arr[i];
+            let newStr = a[0].toUpperCase() + a.slice(1);
+            strArr.push(newStr);
+        }
+        return strArr.join(' ');
+    }
+}
+let str = new MyString();
+console.log(str.reverse('Taalaibek kyzy Gulburak'));
+console.log(str.ucFirst('i want to get more points'));
+console.log(str.ucWords('excuse me, I'm Gulburak'));
